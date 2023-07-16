02b0fb38d20dcdfe962cc30c9789c24624fb25d1
Majid <majid.asfanani@gmail.com>
2023-07-16 12:27:50 +0430

    added expenses_list.

--- lib/expenses.dart
+++ lib/expenses.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'package:expense_tracker/models/expense.dart';
+
 class Expenses extends StatefulWidget {
   const Expenses({super.key});
 
@@ -8,13 +10,25 @@ class Expenses extends StatefulWidget {
 }
 
 class _ExpensesState extends State<Expenses> {
+  final List<Expense> _registeredExpenses = [
+    Expense(
+        title: 'flutter',
+        amount: 19.99,
+        data: DateTime.now(),
+        category: Category.word),
+    Expense(
+        title: 'Cinema',
+        amount: 15.50,
+        data: DateTime.now(),
+        category: Category.leisure),
+  ];
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(body: Column(
-      children: [
-        Text('chart'),
-        Text('data')
-      ],
-    ),);
+    return const Scaffold(
+      body: Column(
+        children: [Text('chart'), Text('data')],
+      ),
+    );
   }
 }
