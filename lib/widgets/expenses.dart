424462f226a6a8baebfa5ee5b6f780d4abefb7a6
Majid <majid.asfanani@gmail.com>
2023-07-17 19:27:17 +0430

    added expense lable input

--- lib/widgets/expenses.dart
+++ lib/widgets/expenses.dart
@@ -1,4 +1,5 @@
 import 'package:expense_tracker/widgets/expenses_list/expenses_list.dart';
+import 'package:expense_tracker/widgets/new_expense.dart';
 import 'package:flutter/material.dart';
 
 import 'package:expense_tracker/models/expense.dart';
@@ -26,7 +27,7 @@ class _ExpensesState extends State<Expenses> {
 
 
 void _openAddExpenseOverlay(){ 
-  showModalBottomSheet(context: context, builder: (ctx) => Text('modol botton sheet'),
+  showModalBottomSheet(context: context, builder: (ctx) => const NewExpense(),
   );
 }
 
