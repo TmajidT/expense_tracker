4125ee107ecf73268378328b21f1febfd756971a
Majid <majid.asfanani@gmail.com>
2023-07-19 12:18:07 +0430

    add expense screen now closes after saving and in higher

--- lib/widgets/expenses.dart
+++ lib/widgets/expenses.dart
@@ -27,6 +27,7 @@ class _ExpensesState extends State<Expenses> {
 
   void _openAddExpenseOverlay() {
     showModalBottomSheet(
+      isScrollControlled: true,
       context: context,
       builder: (ctx) => NewExpense(
         onAddExpense: _addExpense,
--- lib/widgets/new_expense.dart
+++ lib/widgets/new_expense.dart
@@ -49,6 +49,8 @@ class _NewExpenseState extends State<NewExpense> {
         date: _selectedDate!,
         category: _selectedCategory);
     widget.onAddExpense(ep);
+
+    Navigator.pop(context);
   }
 
   @override
@@ -75,7 +77,7 @@ class _NewExpenseState extends State<NewExpense> {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.all(16),
+      padding: const EdgeInsets.fromLTRB(16, 40, 16, 16),
       child: Column(
         children: [
           TextField(
