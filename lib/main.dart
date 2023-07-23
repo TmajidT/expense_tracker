d69686afcaf6b8683b1ec284d463fe3d39d7f241
Majid <majid.asfanani@gmail.com>
2023-07-23 12:49:16 +0430

    more changes

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,21 @@ import 'package:flutter/material.dart';
 
 import 'widgets/expenses.dart';
 
+
+
+var kColorScheme = ColorScheme.fromSeed(seedColor: Color.fromARGB(255, 88, 205, 154));
+
+
 void main() {
   runApp(MaterialApp(
     theme: ThemeData().copyWith(
-        useMaterial3: true,
-        scaffoldBackgroundColor: Color.fromARGB(255, 96, 87, 101)),
+      useMaterial3: true,
+      colorScheme: kColorScheme,
+      appBarTheme: AppBarTheme().copyWith(
+        //backgroundColor: kColorScheme.onPrimaryContainer,
+        foregroundColor: kColorScheme.primaryContainer,
+      ),
+    ),
     debugShowCheckedModeBanner: false,
     home: const Expenses(),
   ));
