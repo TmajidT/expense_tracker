515a05488d1d657cb5bcbcad4a4bf40efc4983e1
Majid <majid.asfanani@gmail.com>
2023-07-23 12:40:33 +0430

    started to change the theme

--- lib/main.dart
+++ lib/main.dart
@@ -2,13 +2,12 @@ import 'package:flutter/material.dart';
 
 import 'widgets/expenses.dart';
 
-
-void main(){
-  runApp(
-    MaterialApp(
-      theme: ThemeData(useMaterial3: true),
-      debugShowCheckedModeBanner: false,
-      home: const Expenses(),
-    )
-  );
+void main() {
+  runApp(MaterialApp(
+    theme: ThemeData().copyWith(
+        useMaterial3: true,
+        scaffoldBackgroundColor: Color.fromARGB(255, 96, 87, 101)),
+    debugShowCheckedModeBanner: false,
+    home: const Expenses(),
+  ));
 }
